ddba1858e557cacf3cddc7851b6afdfd0d072130
Freddy <freddy.artea@gmail.com>
2020-02-27 18:21:11 -0500

    pasando las recetas a su componente

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header'
 import Formulario from './components/Formulario'
+import ListaRecetas from './components/ListaRecetas'
 
 import CategoriasProvider from './context/CategoriasContext'
 import RecetasProvider from './context/RecetasContext'
@@ -16,6 +17,7 @@ function App (){
         <div className='row'>
           <Formulario />
         </div>
+        <ListaRecetas />
       </div>
       </RecetasProvider>
     </CategoriasProvider>
